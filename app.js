192966a92bb49c72f4474a87619bac9ebfcc8d00
VishalSharma06 <vs4258258@gmail.com>
2023-09-21 20:29:36 +0530

    episode-1

---
+++ app.js
@@ -0,0 +1,5 @@
+const heading=React.createElement("h1",{id:"heading",xyz:"abc"},"hello from the React");
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
+//1:04:06 sec
